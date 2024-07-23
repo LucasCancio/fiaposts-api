b75b894a51cac9b1ff22931bc4e701cec979d878
Lucas Cancio <lucas.camargo@energisaterceiros.com.br>
2024-07-23 20:37:45 -0300

    Ajustes em server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import { app } from "@/http/app";
 app.listen(
   {
     port: env.PORT,
+    host: "0.0.0.0",
   },
   (error, address) => {
     if (error) {
+      console.error(error);
       app.log.error(error);
       throw error;
     }
+    console.log(`Server is running on ${address}`);
     app.log.info(`Server is running on ${address}`);
   }
 );
